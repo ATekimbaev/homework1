dfe2d5b7929bfac3cde37f5a89b934dfac794a19
adberg2001 <aaaadyl.disk.erg@gmail.com>
2021-11-07 23:03:19 +0600

    first commit

---
+++ bin/homework.dart
@@ -0,0 +1,30 @@
+void main() {
+//task 1
+  var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  var b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  var result = a + b;
+  for (int i = 0; i < result.length; i++) {
+    print(result[i]);
+    //   if (result[i] == result[i]) {
+    //     print(i);
+    //   }
+  }
+
+  //task 2
+  // var a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+  // for (int i = 0; i < a.length; i++) {
+  //   if (a[i].isEven == true) {
+  //     print(a[i]);
+  //   }
+  // }
+
+  //task 3
+  // String a = 'a';
+  // String b = 'daaart';
+  // var result = b.split('');
+  // for (int i = 0; i < result.length; i++) {
+  //   if (result[i] == a) {
+  //     print(result[i].toString());
+  //   }
+  // }
+}
