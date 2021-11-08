6c1bcadf55ccf906848405279b1f684677accee8
adberg2001 <atekimbaev@atek.kg>
2021-11-08 20:36:05 +0600

    kjlkjlkj

--- bin/homework.dart
+++ bin/homework.dart
@@ -1,30 +1,32 @@
 void main() {
-//task 1
+  task1();
+}
+
+task1() {
   var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
   var b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-  var result = a + b;
-  for (int i = 0; i < result.length; i++) {
-    print(result[i]);
-    //   if (result[i] == result[i]) {
-    //     print(i);
-    //   }
+
+  a.removeWhere((element) => !b.contains(element));
+  a = a.toList();
+  print(a);
+}
+
+task2() {
+  var a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+  for (int i = 0; i < a.length; i++) {
+    if (a[i].isEven == true) {
+      print(a[i]);
+    }
   }
+}
 
-  //task 2
-  // var a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
-  // for (int i = 0; i < a.length; i++) {
-  //   if (a[i].isEven == true) {
-  //     print(a[i]);
-  //   }
-  // }
-
-  //task 3
-  // String a = 'a';
-  // String b = 'daaart';
-  // var result = b.split('');
-  // for (int i = 0; i < result.length; i++) {
-  //   if (result[i] == a) {
-  //     print(result[i].toString());
-  //   }
-  // }
+task3() {
+  String a = 'a';
+  String b = 'daaart';
+  var result = b.split('');
+  for (int i = 0; i < result.length; i++) {
+    if (result[i] == a) {
+      print(result[i].toString());
+    }
+  }
 }
